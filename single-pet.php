7a80c041637e7a7d4275d8c438bc03b9b1e4e672
Avia Aharon <avia@google.com>
2015-08-08 16:00:48 +0300

    Pet single: remove default thumbnail section

--- single-pet.php
+++ single-pet.php
@@ -30,15 +30,8 @@ get_header(); ?>
         </ul>
       <?php endif; ?>
 
-      <?php if ( has_post_thumbnail() ) : ?>
-        <div class="row">
-          <div class="column">
-            <?php the_post_thumbnail( '', array('class' => 'th') ); ?>
-          </div>
-        </div>
-      <?php endif; ?>
-
       <?php the_content(); ?>
+
       </div>
       <footer>
         <?php wp_link_pages( array('before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ), 'after' => '</p></nav>' ) ); ?>
