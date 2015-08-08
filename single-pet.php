eacaa296168c4ed0dfeeb107bcea4a11544fe866
Sagie Maoz <sagiem@gmail.com>
2015-08-08 15:41:17 +0300

    Full-width pet page

--- single-pet.php
+++ single-pet.php
@@ -10,7 +10,7 @@
 get_header(); ?>
 
 <div class="row">
-  <div class="small-12 large-8 columns" role="main">
+  <div class="small-12 large-12 columns" role="main">
 
   <?php while ( have_posts() ) : the_post(); ?>
     <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
