5ea3bf8286ef9ddb095f6f5e7dbd689771f02152
Sagie Maoz <sagiem@gmail.com>
2015-10-18 17:21:02 +0300

    Pet boxes: Link to pet page

--- parts/box-pet.php
+++ parts/box-pet.php
@@ -1,14 +1,16 @@
-<?php
-$pictures = get_field('pictures');
-if( $pictures ): ?>
-  <div class="box-picture">
-    <img src="<?= $pictures[0]['url'] ?>" alt="<?php echo $pictures[0]['alt']; ?>" />
-  </div>
-<?php endif; ?>
+<a href="<?php the_permalink() ?>" title="<?php the_title_attribute() ?>">
+  <?php
+  $pictures = get_field('pictures');
+  if( $pictures ): ?>
+    <div class="box-picture">
+      <img src="<?= $pictures[0]['url'] ?>" alt="<?php echo $pictures[0]['alt']; ?>" />
+    </div>
+  <?php endif; ?>
 
-<div class="pet-details">
-  <h2><?php the_title(); ?></h2>
-  <div class="pet-description">
-    <?php the_content(); ?>
+  <div class="pet-details">
+    <h2><?php the_title(); ?></h2>
+    <div class="pet-description">
+      <?php the_content(); ?>
+    </div>
   </div>
-</div>
+</a>
