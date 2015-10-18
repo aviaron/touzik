2b6fa72dbcade6825c7d855e424e567e22fc718f
Sagie Maoz <sagiem@gmail.com>
2015-10-18 12:05:39 +0300

    Add bold style for Alef font

--- library/enqueue-scripts.php
+++ library/enqueue-scripts.php
@@ -16,7 +16,7 @@ if ( ! function_exists( 'foundationpress_scripts' ) ) :
 	// Enqueue the main Stylesheet.
 	wp_enqueue_style( 'main-stylesheet', get_stylesheet_directory_uri() . '/css/foundation.css' );
 
-  wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700|Alef' );
+  wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700|Alef:400,700' );
 
 	// Deregister the jquery version bundled with WordPress.
 	wp_deregister_script( 'jquery' );
