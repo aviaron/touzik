f237af9407faa8435f72b062ac96582be9643ffb
Avia Aharon <avia@google.com>
2015-10-18 11:41:17 +0300

    Add Alef font again

--- library/enqueue-scripts.php
+++ library/enqueue-scripts.php
@@ -15,7 +15,7 @@ if ( ! function_exists( 'foundationpress_scripts' ) ) :
 	// Enqueue the main Stylesheet.
 	wp_enqueue_style( 'main-stylesheet', get_template_directory_uri() . '/assets/stylesheets/foundation.css', array(), '2.6.1', 'all' );
 
-  wp_enqueue_style( 'alef-font', '//fonts.googleapis.com/earlyaccess/alefhebrew.css' );
+  wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Open+Sans+Condensed:300|Alef' );
 
 	// Deregister the jquery version bundled with WordPress.
 	wp_deregister_script( 'jquery' );
