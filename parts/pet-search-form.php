369a3e26203628f1bfbb7d77a383a82bc8be584f
Sagie Maoz <sagiem@gmail.com>
2015-11-01 13:57:10 +0200

    Add breed, size fields + searchj

--- parts/pet-search-form.php
+++ parts/pet-search-form.php
@@ -1,12 +1,25 @@
+<?php
+
+function search_options_for($name, $default_label) {
+  $field = acf_get_local_field( "field_$name" );
+?>
+<label for="<?= $name ?>"><?= $field['label'] ?>:</label>
+<select name="<?= $name ?>">
+  <option selected value="">-- <?= $default_label ?> --</option>
+  <?php foreach( $field['choices'] as $key => $value ): ?>
+    <option value="<?= $key ?>"><?= $value ?></option>
+  <?php endforeach; ?>
+</select>
+<?php
+}
+
+?>
+
 <form action="<?= admin_url( 'admin-ajax.php' ) ?>" id="pet-search-form">
 
-<input type="hidden" name="action" value="pet-search" />
-
-מין:
-<select name="sex">
-<option selected value="">-- כל המינים --</option>
-<option value="male">זכר</option>
-<option value="female">נקבה</option>
-</select>
+  <input type="hidden" name="action" value="pet-search" />
+  <?php search_options_for('sex', 'כל המינים'); ?>
+  <?php search_options_for('size', 'כל הגדלים'); ?>
+  <?php search_options_for('breed', 'כל הגזעים'); ?>
 
 </form>
