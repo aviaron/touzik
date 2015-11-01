5d850a80e5d335d57bc321e827aade916c0b7f66
Sagie Maoz <sagiem@gmail.com>
2015-11-01 14:11:36 +0200

    Disable search form while loading

--- js/custom/pet-search.js
+++ js/custom/pet-search.js
@@ -1,6 +1,7 @@
 $(function(){
 
 	var $form = $('#pet-search-form'),
+			$inputs = $form.find(':input'),
 			$container = $('.homepage-box-list');
 
 	$form.on('submit', function(e) {
@@ -9,9 +10,12 @@ $(function(){
 		var action = $(this).attr('action'),
 				params = $(this).serialize();
 
+		$inputs.attr('disabled', true);
+
 		$.get(action, params).then(function(output){
 			$container.fadeOut('fast', function(){
 				$(this).html(output).fadeIn('fast');
+				$inputs.attr('disabled', false);
 			});
 		});
 	});
