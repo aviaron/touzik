251850f069b71a3bb9768fe04906d49586f36adf
Sagie Maoz <sagiem@gmail.com>
2015-11-01 12:37:30 +0200

    AJAX pet search functionality
    TODO admin only?

--- functions.php
+++ functions.php
@@ -54,6 +54,8 @@ require_once( 'library/custom-header.php' );
 /** Add Header image */
 require_once( 'library/facebook-sdk.php' );
 
+require_once( 'library/pet-search.php' );
+
 /** Admin actions controller */
 if ( current_user_can( 'edit_others_posts' ) ) {
   require_once( 'library/admin-controller.php');
