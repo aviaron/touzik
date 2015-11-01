dd0e0e97046ff517fa84e1c95a361a7da040f0c8
Sagie Maoz <sagiem@gmail.com>
2015-11-01 18:30:06 +0200

    When no results, show 3 random pet items

--- library/pet-search.php
+++ library/pet-search.php
@@ -3,9 +3,22 @@
 add_action( 'wp_ajax_pet-search', 'pet_search' );
 add_action( 'wp_ajax_nopriv_pet-search', 'pet_search' );
 
-function pet_search() {
+function render_results($posts) {
 	global $post;
 
+	foreach( $posts as $post ) {
+		setup_postdata( $post );
+?>
+<li <?php post_class( 'small-1 large-4 columns' ) ?> id="post-<?php the_ID(); ?>">
+	<div class="homepage-box">
+		<?php get_template_part( 'parts/box', 'pet' ); ?>
+	</div>
+</li>
+<?php
+	}
+}
+
+function pet_search() {
 	$meta_query = array(
 		'relation' => 'AND'
 	);
@@ -21,24 +34,27 @@ function pet_search() {
 	}
 
 	$posts = get_posts(array(
-		'posts_per_page'  => 50,
+		'posts_per_page' => 50,
 		'post_type' => 'pet',
 		'orderby' => 'ID',
 		'order' => 'DESC',
 		'meta_query' => $meta_query
 	));
 
-	// TODO: deal with zero results?
-
-	foreach( $posts as $post ) {
-		setup_postdata( $post );
+	if ( $posts ) {
+		render_results($posts);
+	} else {
+		$posts = get_posts(array(
+			'posts_per_page' => 3,
+			'post_type' => 'pet',
+			'orderby' => 'rand'
+		));
 ?>
-<li <?php post_class( 'small-1 large-4 columns' ) ?> id="post-<?php the_ID(); ?>">
-	<div class="homepage-box">
-		<?php get_template_part( 'parts/box', 'pet' ); ?>
-	</div>
-</li>
+	<li class="no-results small-1 large-12 columns">
+		לא נמצאו תוצאות על פי פרמטרי החיפוש. אולי הכלבים הבאים יענינו אותך:
+	</li>
 <?php
+		render_results($posts);
 	}
 
 	exit;
