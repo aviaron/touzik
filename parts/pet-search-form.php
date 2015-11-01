d8af1901c3ea9fefaf8afdf961ca2cc81b356621
Sagie Maoz <sagiem@gmail.com>
2015-11-01 17:51:09 +0200

    Style pet search form

--- parts/pet-search-form.php
+++ parts/pet-search-form.php
@@ -4,7 +4,8 @@ function search_options_for($name, $default_label) {
   $field = acf_get_local_field( "field_$name" );
 ?>
 <select name="<?= $name ?>">
-  <option selected value=""><?= $field['label'] ?>:</option>
+  <option selected value=""><?= $field['label'] ?></option>
+  <option disabled>─</option>
   <?php foreach( $field['choices'] as $key => $value ): ?>
     <option value="<?= $key ?>"><?= $value ?></option>
   <?php endforeach; ?>
@@ -17,8 +18,8 @@ function search_options_for($name, $default_label) {
 <form action="<?= admin_url( 'admin-ajax.php' ) ?>" id="pet-search-form">
 
   <input type="hidden" name="action" value="pet-search" />
+  <?php search_options_for('breed', 'כל הגזעים'); ?>
   <?php search_options_for('sex', 'כל המינים'); ?>
   <?php search_options_for('size', 'כל הגדלים'); ?>
-  <?php search_options_for('breed', 'כל הגזעים'); ?>
 
 </form>
