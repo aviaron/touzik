7dc9f1fc732850a44b748e888022dd98e4ec0bf4
Sagie Maoz <sagiem@gmail.com>
2016-01-10 19:10:59 +0200

    Share buttons style

--- library/pet-share.php
+++ library/pet-share.php
@@ -42,7 +42,7 @@ $share_links = array(
 
 ?>
 
-<ul class="entry-share">
+<ul class="entry-share inline-list">
   <li>שתף:</li>
   <li><a href="<?= $share_links['mail'] ?>"><i class="fa fa-envelope"></i></a></li>
   <li><a href="<?= $share_links['facebook'] ?>" target="_blank"><i class="fa fa-facebook"></i></a></li>
